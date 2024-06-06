f57cd46dcff0465c1a77b9169d013305c802d437
Adhish Bahl <adhishbahl0@gmail.com>
2024-06-06 19:27:25 +0530

    about done

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import '../style/About.css'
 
 function About() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div className='about'>
+
+			<div className="parallax parallax1">
+				<div className="cont">
+					<div className="heading">What we do?</div>
+					<div className="desc">The Christ University Alumni Association serves as a vital bridge between Christ University and its former students, fostering a lifelong relationship built on mutual benefit and support. The association aims to create a robust network of alumni who can contribute to the university's growth and development while also benefitting from continued engagement with their alma mater. CUAA organizes various events, including reunions, networking opportunities, and professional development workshops, which help alumni stay connected with each other and the university. Additionally, the association plays a significant role in mentoring current students, providing career guidance, and facilitating internships and job placements through its extensive network. By engaging in philanthropic activities and supporting university initiatives, the Christ University Alumni Association also contributes to the institution's mission of academic excellence and holistic </div>
+				</div>
+			</div>
+
+			<div className='justLine'></div>
+
+			<div className="parallax parallax2">
+				<div className="cont">
+					<div className="heading">About the University</div>
+					<div className="desc">CHRIST (Deemed to be University) was born out of the educational vision of St Kuriakose Elias Chavara, an educationist and a social reformer of the nineteenth century in South India. He founded the first indigenous Catholic congregation, Carmelites of Mary Immaculate (CMI) in 1831 which administers CHRIST (Deemed to be University).
+
+						Established in 1969 as Christ College, the Institution undertook path-breaking initiatives in Indian higher education with the introduction of innovative and modern curricula, insistence on academic discipline, imparting of Holistic Education and adoption of global higher education practices with the support of creative and dedicated staff. The University Grants Commission (UGC) of India conferred Autonomy to Christ College vide its notification F.13-1/2004(Desk-AC) dated 7 October 2004 and identified it as an Institution with Potential for Excellence vide its notification No. F. 121/2004(AIS/PE) dated 19 May 2006. In 2008 under Section 3 of the UGC Act, 1956, the Ministry of Human Resource Development (MHRD) of the Government of India, declared the Institution a Deemed to be University, vide its notification No. F. 9-34/2007- U.3(A) dated 22 July 2008 in the name and style of ‘Christ University’. One of the first institutions in India to be accredited in 1998 by the National Assessment and Accreditation Council (NAAC), and subsequently in 2004 and 2016, the Institution has the top Grade ‘A’ on a 4-point scale. As per the direction of MHRD, vide its notification no. F.9-34/2007-U.3(A), dated 11 January 2018 the name of the Institution was changed from ‘Christ University’ to ‘CHRIST (Deemed to be University)’. CHRIST (Deemed to be University)</div>
+				</div>
+			</div>
+
+			<div className='justLine'></div>
+
+			<div className="parallax parallax3">
+				<div className="cont">
+					<div className="heading">What is our Focus point?</div>
+					<div className="desc">The multi-disciplinary Institution which focuses on teaching, research and service, offers Bachelor to Doctoral programmes in Humanities, Social Sciences, Science, Commerce, Management, Engineering, Law, Education, and Architecture to over 25000 students. The three campuses of the University at Bengaluru in Karnataka, Pune Lavasa in Maharashtra, and Delhi National Capital Region (NCR) are a living example of harmonious multiculturalism with students from all the states and Union Territories of India and about 60 different countries. CHRIST (Deemed to be University) publishes six peer-reviewed research journals and has published more than 300 books in Kannada and English. A promoter of sports, music and literary activities, it is a nurturing ground for creative excellence.</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default About
